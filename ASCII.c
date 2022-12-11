6a5a4f743758962c1b4473e973375e9b6022ae3b
Asmi Anand <120274463+anandasmi@users.noreply.github.com>
2022-12-11 20:30:59 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char c;
+    printf("Enter the character = ");
+    scanf("%c", &c);
+    printf("The ASCII Value of %c = %d", c, c);
+}
---
+++ changedoubleintointbeforeadd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    double a = 5.67;
+    int b = 9;
+
+    double result = (int)a + b;
+    printf("%lf", result);
+
+    return 0;
+}
