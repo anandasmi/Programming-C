91397f3dd778365f8195c568acbed8fefa07639f
Asmi Anand <120274463+anandasmi@users.noreply.github.com>
2022-12-11 19:49:06 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, sum;
+    a = 2;
+    b = 2;
+    sum = a + b;
+    printf("The sum is = ");
+    printf("%d", sum);
+}
---
+++ addusingscanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n1, n2, sum;
+    printf("Enter the numbers =");
+    scanf("%d %d", &n1, &n2);
+    sum = n1 + n2;
+    printf("The sum is = ");
+    printf("%d", sum);
+}
