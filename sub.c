19b71cebe0de18d6898bad0d163f75a0213d5230
Asmi Anand <120274463+anandasmi@users.noreply.github.com>
2022-12-14 17:47:02 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, sub;
+    a = 2;
+    b = 2;
+    sub = a - b;
+    printf("The answer is = ");
+    printf("%d", sub);
+}
---
+++ subusingscanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, sub;
+    printf("Enter the numbers = ");
+    scanf("%d %d", &a, &b);
+    sub = a - b;
+    printf("The answer is = ");
+    printf("%d", sub);
+}
