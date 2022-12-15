e83b7edafde01596ea89272615c2146b85cb2173
Asmi Anand <120274463+anandasmi@users.noreply.github.com>
2022-12-15 19:09:54 +0530

    Add files via upload

---
+++ trueorfalse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    bool value1 = true;
+    bool value2 = false;
+
+    printf("%d", value1);
+    printf("\n%d", value2);
+
+    return 0;
+}
